1f71cb625f7d1c1858cd496b083327f54db870a7
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2023-02-17 11:15:36 +0600

    Create Number Method.js

---
+++ JavaScript2023/Number Method.js
@@ -0,0 +1,47 @@
+var name = "Arfin Shariar"; //This is a String Datatype
+console.log(typeof(name));
+var name2 = "Ashraful Islam"; //This is a String Datatype
+console.log(name2);
+var age = 23; //This is a Number Datatype
+console.log(age);
+
+//using of ParseInt
+var age2 = "20";
+age2 = parseInt(age2);
+console.log(typeof(age2)); //output: number
+
+//using of Parsefloat
+var length = "20.03";
+length = parseFloat(length);
+console.log(typeof(length)); //output: number
+
+//toFixed Method
+var value = 2.14512;
+console.log(value.toFixed(2));
+
+//toPrecision Method
+var value2 = 2.5678;
+console.log(value2.toPrecision(2));
+
+//Number using Console
+console.log(Number("120"));
+
+//variable Declare
+var name1 = "Arfin Shariar";
+var name2 = "Maisha Rahman";
+var name3 = "Lamia Siddique";
+var name4 = "Shakil Ahmed";
+
+var age1 = 12;
+var age2 = 13;
+var age3 = 14;
+var age4 = 15;
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
+console.log(name4);
+console.log(age1);
+console.log(age2);
+console.log(age3);
+console.log(age4);
